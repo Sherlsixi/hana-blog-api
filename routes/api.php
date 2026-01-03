64c7a706286b158776442d670c576bbb60a628ef
xiaoxiao <sixihana@gmail.com>
2026-01-03 11:12:43 +0900

    PostController&model作成

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,7 +8,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/hello', function(){
+Route::get('/hello', function () {
     return ['message' => 'Hello Laravel12'];
 });
 
+Route::apiResource('posts',PostController::class);
